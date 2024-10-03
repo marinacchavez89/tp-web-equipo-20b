48618f0666a29a4a27543e8cf9d4e3ee4368cb2d
Martin Lozano <martin.lozano995@gmail.com>
2024-10-03 19:23:31 -0300

    Se agrega page de registro de cliente y un formulario para rellenar

---
+++ TPWeb_equipo-20B/RegistroCliente.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="RegistroCliente.aspx.cs" Inherits="TPWeb_equipo_20B.Formulario_web1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1 class="text-center my-4">Registro de Cliente</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-lg">
+                    <div class="card-header bg-info text-white text-center">
+                        Ingrese sus datos para participar
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="txtDNI" class="form-label" >DNI</label>
+                               <asp:TextBox ID="txtDNI" runat="server" CssClass="form-control" placeholder="Ingrese su DNI"></asp:TextBox>
+                        </div>
+                        <div>
+                            <asp:Button ID="btnVerificar" runat="server" CssClass="btn btn-info" Text="Verificar" />
+                        </div>
+                        <div class="mb-3">
+                            <label for="txtNombre" class="form-label">Nombre:</label>
+                              <asp:TextBox ID="txtNombre" runat="server" CssClass="form-control" Enabled="false" placeholder="Ingrese su Nombre"></asp:TextBox>
+                        </div>
+                          <div class="mb-3">
+                            <label for="txtApellido" class="form-label">Apellido:</label>
+                            <asp:TextBox ID="txtApellido" runat="server" CssClass="form-control" placeholder="Ingrese su apellido" Enabled="false"></asp:TextBox>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="txtEmail" class="form-label">Email:</label>
+                            <asp:TextBox ID="txtEmail" runat="server" CssClass="form-control" placeholder="Ingrese su email" Enabled="false"></asp:TextBox>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="txtTelefono" class="form-label">Teléfono:</label>
+                            <asp:TextBox ID="txtTelefono" runat="server" CssClass="form-control" placeholder="Ingrese su número de teléfono" Enabled="false"></asp:TextBox>
+                        </div>
+                        <div class="text-center">
+                            <asp:Button ID="btnParticipar" runat="server" Text="PARTICIPAR" CssClass="btn btn-succes" Enabled="false" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ TPWeb_equipo-20B/RegistroCliente.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPWeb_equipo_20B
+{
+
+
+    public partial class Formulario_web1
+    {
+
+        /// <summary>
+        /// Control txtDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDNI;
+
+        /// <summary>
+        /// Control btnVerificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVerificar;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+
+        /// <summary>
+        /// Control btnParticipar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnParticipar;
+    }
+}
